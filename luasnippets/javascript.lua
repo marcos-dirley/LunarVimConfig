16a78e5f85087a7e232a245f2619264d0a4c1b1a
Marcos Dirley <marcosdirley@gmail.com>
2024-02-18 22:00:53 -0300

    adds js snippets

---
+++ luasnippets/javascript.lua
@@ -0,0 +1,9 @@
+return {
+    s("log", { t "console.log" }),
+    s("getId", { t "document.getElementById" }),
+    s("getName", { t "document.getElementsByName" }),
+    s("getTag", { t "document.getElementsByTagName" }),
+    s("getClass", { t "document.getElementsByClassName" }),
+    s("query", { t "document.querySelector" }),
+    s("queryAll", { t "document.querySelectorAll" }),
+}
